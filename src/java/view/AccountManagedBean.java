dfecf3aef109cd021c437ad7de930ba275afee7a
BielH22 <31714430+BielH22@users.noreply.github.com>
2017-10-05 10:32:04 -0300

    Second

--- src/java/view/AccountManagedBean.java
+++ src/java/view/AccountManagedBean.java
@@ -4,23 +4,33 @@ import model.Account;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import controller.AccountJpaController;
+import java.util.ArrayList;
 
 @ManagedBean
 @RequestScoped
 
 public class AccountManagedBean {
 
+    private ArrayList<Account> listAccounts = new ArrayList<>();
+
     private Account ActualAccount = new Account();
     private AccountJpaController controlAccount = new AccountJpaController(EmProvider.getInstance().getEntityManagerFactory());
+
     public AccountManagedBean() {
     }
 
-    public void saveAccount() {
+    public void listAccounts() {
+        listAccounts = new ArrayList(controlAccount.findAccountEntities());
+    }
+
+    public String saveAccount() {
         try {
             controlAccount.create(ActualAccount);
+            listAccounts();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return "ManageAccounts.xhtml";
     }
 
     public Account getActualAccount() {
@@ -30,4 +40,13 @@ public class AccountManagedBean {
     public void setActualAccount(Account actualAccount) {
         this.ActualAccount = actualAccount;
     }
+
+    public ArrayList<Account> getListAccounts() {
+        return listAccounts;
+    }
+
+    public void setListAccounts(ArrayList<Account> listAccounts) {
+        this.listAccounts = listAccounts;
+    }
+
 }
